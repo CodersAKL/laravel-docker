b1f647a44977f1fb57e147082daa2485e0d89404
FDiskas <projektas@gmail.com>
2016-07-02 23:37:41 +0300

    Some pimp on structure

--- readme.md
+++ readme.md
@@ -11,19 +11,15 @@
 	`php artisan make:migration:schema add_user_id_to_posts_table --schema="user_id:integer"`
 1. Create relations
 
-	1. Many to many
-
+	* Many to many
 		`php artisan make:migration:pivot tags pages`
-	1. One to many
-
+	* One to many
 		`php artisan make:migration:schema create_companies_table --schema="user_id:integer:foreign:unsignption:text,active:boolean,tax_no:varchar,child:boolean,child_of:integer:unsign"`
 
 # Translations
-1. To translate php files
-	1. Visit url public/_translations
-	1. To sync files with db
+To translate php files Visit url public/_translations
 
-	`php artisan translation:fetch`
-	1. To export back to files
-
-	`php artisan translation:dump`
+* To sync files with db
+    `php artisan translation:fetch`
+* To export back to files
+    `php artisan translation:dump`
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,11 +52,11 @@
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <a id="dLabel" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
-                            <ul class="dropdown-menu" role="menu">
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
